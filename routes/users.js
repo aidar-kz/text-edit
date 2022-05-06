c35a28746965f240353f7e483e7afb3af7d82f35
Aydeke <aydeke2@gmail.com>
2022-05-06 20:02:26 +0600

    Add LocalStrategy

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const passport = require('passport')
+const crypto = require('crypto')
 const LocalStrategy = require('passport-local')
 const router = express.Router()
 
 passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    User.findOne({ email }, (err, user) => {
+        if (err) return done(err)
+        if (!user) return done(null, false, { message: 'Неверное имя пользователя или пароль' })
 
+        crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', (err, hashedPassword) => {
+            if (err) return done(err)
+            if (!crypto.timingSafeEqual(user.hashedPassword, hashedPassword)) {
+                return done(null, false, { message: 'Неверный пароль' })
+            }
+            return done(null, user)
+        })
+    })
 }))
 
 router.get('/register', (req, res) => res.render('user-register'))
